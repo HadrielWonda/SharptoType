fbf94c8904eb26a9689a423af9c130acd863b34c
Olawale Abaniwonda <olawalebash2017@gmail.com>
2022-08-30 01:55:10 +0100

    Update generators.ts

--- src/generators.ts
+++ src/generators.ts
@@ -2,6 +2,7 @@ import * as vscode from 'vscode';
 import * as types from './types';
 import { ExtensionConfig } from "./config";
 
+
 import { CSharpProperty } from "./properties";
 import { CSharpMethod, CSharpParameter, CSharpConstructor, CSharpRecord } from "./methods";
 import { CSharpClass } from "./classes";
